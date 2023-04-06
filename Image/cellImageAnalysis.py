cedc031ee81838ef860f32aa239b0fd153afe50b
Gabor Turu <gaborturu@gmail.com>
2023-04-06 17:27:02 +0200

    cell detector created

--- Image/cellImageAnalysis.py
+++ Image/cellImageAnalysis.py
@@ -2,6 +2,13 @@ import matplotlib.pyplot as plt
 from skimage import io
 import numpy as np
 
+from skimage.filters import threshold_mean
+from skimage.morphology import binary_closing, binary_opening
+from skimage.measure import label
+from scipy import ndimage as ndi
+from skimage.segmentation import watershed
+from skimage.feature import peak_local_max
+
 class Image:
     
     def __init__(self, path, channel_names=None):
@@ -22,6 +29,47 @@ class Image:
             if self.channel_names != None:
                 plt.title(self.channel_names[i])
             plt.axis("off")
+            
+    def remove_background(self):
+        pass
+    
+class XpressImage(Image):
+    pass
+            
+class Detector:
+    
+    def __init__(self, cell_channel=None, nucleus_channel=None):
+        self.cell_channel = cell_channel
+        self.nucleus_channel = nucleus_channel
+        
+        
+    def watershed(self, binary):
+        pass
+    
+    def make_binary(self, image):
+        pass
+        
+    def detect_cells(self, image):
+        
+        image_data = image[:,:,self.cell_channel]
+        thr = threshold_mean(image_data)
+        binary = image_data > thr
+        binary = binary_closing(binary, np.ones(shape=(8,8)))
+        binary = binary_opening(binary, np.ones(shape=(15,15)))
+        distance = ndi.distance_transform_edt(binary)
+        coords = peak_local_max(distance, footprint=np.ones((150, 150)), labels=binary)
+        mask = np.zeros(distance.shape, dtype=bool)
+        mask[tuple(coords.T)] = True
+        markers, _ = ndi.label(mask)
+        labels = watershed(-distance, markers, mask=binary)
+        
+        return labels
+    
+    def detect_nuclei(self, image):
+        pass
+    
+    def detect_cell_borders(self, image):
+        pass            
             
 class Experiment:
     
